75d5513a3f607097f547a72dac98aa95d3f2a3ab
Marc Sola <mmarcsola@gmail.com>
2017-01-20 20:23:01 +0100

    urls

--- laravelapp1/routes/web.php
+++ laravelapp1/routes/web.php
@@ -10,7 +10,37 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
+#delroigtwoerijw
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function()
+{
+    return view('auth.login');
+});
+
+Route::get('/logout', function()
+{
+    return 'logout usuario';
+});
+
+Route::get('/catalog', function()
+{
+    return view('catalog.index');
+});
+
+Route::get('/catalog/show/{id}', function($id)
+{
+    return view('catalog.show', array('id'=>$id));
+});
+
+Route::get('/catalog/create', function()
+{
+    return view('catalog.create');
+});
+
+Route::get('/catalog/edit/{id}', function($id)
+{
+    return view('catalog.edit', array('id'=>$id));
 });
